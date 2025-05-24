b7406b15c673961943fb9ad0cb0e69c6e61fda63
amitkumardss012 <amitkumaryadav62029@gmail.com>
2025-05-24 19:54:29 +0530

    second

--- public/api/routes/admin.routes.js
+++ public/api/routes/admin.routes.js
@@ -12,7 +12,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const controllers_1 = require("../controllers");
 const auth_middleware_1 = require("../middleware/auth.middleware");
 const adminRoute = (app) => __awaiter(void 0, void 0, void 0, function* () {
-    app.post("/create", { preHandler: [auth_middleware_1.authenticatAdmin] }, controllers_1.AdminController.createAdmin);
+    app.post("/create", 
+    // { preHandler: [authenticatAdmin] },
+    controllers_1.AdminController.createAdmin);
     app.post("/login", controllers_1.AdminController.login);
     app.get("/all", { preHandler: auth_middleware_1.authenticatAdmin }, controllers_1.AdminController.getAllAdmin);
     app.get("/logout", { preHandler: auth_middleware_1.authenticatAdmin }, controllers_1.AdminController.logout);
